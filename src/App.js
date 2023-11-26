ad870ea009d3be5cf593b676cfff5562666bed4e
Sheila Nyambura <sheilanyambura394@gmail.com>
2023-11-26 16:13:56 +0300

    created a function that handles api request

--- src/App.js
+++ src/App.js
@@ -2,3 +2,13 @@ import React, { useState, useEffect } from 'react';
 import BotCollection from './components/Botcollection';
 import Botarmy from './components/Botarmy';
 import './App.css';
+
+  useEffect(() => {
+    fetchBots();
+  }, []);
+
+  function fetchBots() {
+    fetch('https://mock-4oke.onrender.com/bots')
+      .then((r) => r.json())
+      .then((bots) => setBots(bots));
+  }
